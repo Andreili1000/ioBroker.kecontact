3ea551656cc66e1d39bbc7002682a0f954c57792
Andreili1000 <60061677+Andreili1000@users.noreply.github.com>
2020-02-02 20:14:37 +0100

    bugfix

--- kecontact.js
+++ kecontact.js
@@ -619,6 +619,8 @@ function handleMessage(message) {
         if (states[key]) {
             try {
                 // redirect identical state naming in reports >=100 with standard reports 1-3
+                adapter.log.info("reportID " + adapter.namespace.reportID);
+                adapter.log.info("states-key " + states[key]);
                 if (adapter.namespace.reportID>=100 && states[key]=="E pres"){
                   updateState(states["Sess E pres"], message[key]);
                 } else if  (adapter.namespace.reportID>=100 && states[key]=="Curr HW"){
